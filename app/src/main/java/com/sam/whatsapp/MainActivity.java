088dcc4dc2b5bd293f7b1492eae8a0311206a0fb
Samridhi <samridhi18arora@gmail.com>
2023-03-26 21:47:01 +0530

    Commit

--- app/src/main/java/com/sam/whatsapp/MainActivity.java
+++ app/src/main/java/com/sam/whatsapp/MainActivity.java
@@ -1,14 +1,61 @@
 package com.sam.whatsapp;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.sam.whatsapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        auth = FirebaseAuth.getInstance();
+        setContentView(binding.getRoot());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.setting:
+                Toast.makeText(this, "Setting clicked", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.logout:
+                auth.signOut();
+                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(intent);
+
+
+        }
+
+
+        return true;
+
     }
 }
--- app/src/main/java/com/sam/whatsapp/SignInActivity.java
+++ app/src/main/java/com/sam/whatsapp/SignInActivity.java
@@ -1,14 +1,98 @@
 package com.sam.whatsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.sam.whatsapp.databinding.ActivitySignInBinding;
+import com.sam.whatsapp.databinding.ActivitySignupBinding;
 
 public class SignInActivity extends AppCompatActivity {
+
+    ActivitySignInBinding binding;
+    ProgressDialog progressDialog;
+    FirebaseAuth auth;
+    GoogleSignInClient mgoogleSignInClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding=ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+
+        auth=FirebaseAuth.getInstance();
+        progressDialog= new ProgressDialog(SignInActivity.this);
+        progressDialog.setTitle("Login");
+        progressDialog.setMessage("We are login your Account");
+
+        GoogleSignInOptions gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                        .requestEmail()
+                                .build();
+
+        mgoogleSignInClient= GoogleSignIn.getClient(this,gso);
+
+
+        binding.btnsSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                progressDialog.show();
+                auth.signInWithEmailAndPassword(binding.etEmail.getText().toString(),binding.etPassword.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+
+                        if(task.isSuccessful()){
+                            Intent intent= new Intent(SignInActivity.this,MainActivity.class);
+                            startActivity(intent);
+                        }
+
+                        else{
+                            Toast.makeText(SignInActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+
+            }
+        });
+
+        binding.txtClickForSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(SignInActivity.this,SignupActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+       if(auth.getCurrentUser()!=null){
+           Intent intent= new Intent(SignInActivity.this,MainActivity.class);
+            startActivity(intent);
+
+        }
+
+
+
     }
 }
