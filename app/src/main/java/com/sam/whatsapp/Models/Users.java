fe3e1f48a9fc4723d6d3367a54d9e11c6230540f
Samridhi <samridhi18arora@gmail.com>
2023-03-26 21:47:01 +0530

    Commit

---
+++ app/src/main/java/com/sam/whatsapp/Models/Users.java
@@ -0,0 +1,73 @@
+package com.sam.whatsapp.Models;
+
+public class Users {
+
+    String profilepic, username, mail, password ,userId, lastMessage;
+
+    public Users(String profilepic, String username, String mail, String password, String userId, String lastMessage) {
+        this.profilepic = profilepic;
+        this.username = username;
+        this.mail = mail;
+        this.password = password;
+        this.userId = userId;
+        this.lastMessage = lastMessage;
+    }
+
+    public Users(){}
+
+    //signup constructor
+    public Users(String username, String mail, String password) {
+        this.username = username;
+        this.mail = mail;
+        this.password = password;
+    }
+
+
+    public String getProfilepic() {
+        return profilepic;
+    }
+
+    public void setProfilepic(String profilepic) {
+        this.profilepic = profilepic;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getLastMessage() {
+        return lastMessage;
+    }
+
+    public void setLastMessage(String lastMessage) {
+        this.lastMessage = lastMessage;
+    }
+}
--- app/src/main/java/com/sam/whatsapp/SignupActivity.java
+++ app/src/main/java/com/sam/whatsapp/SignupActivity.java
@@ -1,14 +1,86 @@
 package com.sam.whatsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.sam.whatsapp.Models.Users;
+import com.sam.whatsapp.databinding.ActivitySignupBinding;
 
 public class SignupActivity extends AppCompatActivity {
+
+    ActivitySignupBinding binding;
+    private FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        binding=ActivitySignupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+        mAuth = FirebaseAuth.getInstance();
+        database=FirebaseDatabase.getInstance("https://whatsapp-35c27-default-rtdb.asia-southeast1.firebasedatabase.app");
+        progressDialog= new ProgressDialog(SignupActivity.this);
+        progressDialog.setTitle("Creating Account");
+        progressDialog.setMessage("We are creating your Account");
+
+
+        binding.btnsSignUP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressDialog.show();
+
+                mAuth.createUserWithEmailAndPassword(binding.txtEmail.getText().toString(),binding.txtPassword.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.hide();
+
+                        if (task.isSuccessful()){
+
+
+                            Users user=new Users(binding.txtName.getText().toString(),binding.txtEmail.getText().toString(),binding.txtPassword.getText().toString());
+
+                           String id=task.getResult().getUser().getUid();
+                         //  Log.d("TAG","VALUE OF ID"+id);
+                            database.getReference().child("users").child(id).setValue(user);
+
+                            Toast.makeText(SignupActivity.this, "User created successfully", Toast.LENGTH_SHORT).show();
+
+
+                        }
+                        else{
+                            Toast.makeText(SignupActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+                
+            }
+        });
+
+        binding.txtAlready.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(SignupActivity.this,SignInActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
